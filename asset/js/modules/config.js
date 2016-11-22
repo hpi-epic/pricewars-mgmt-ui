698d32d047bf8231a53b4f4338bf4b97a4741543
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2016-11-22 15:48:39 +0100

    Remove double slashes for all services

--- asset/js/modules/config.js
+++ asset/js/modules/config.js
@@ -14,8 +14,8 @@
             function ($route, $routeParams, $location, $http, $scope, $cookieStore, $window, $filter, $rootScope) {
 
               $scope.consumer                     = {};
-              $scope.consumer_url                 = "http://vm-mpws2016hp1-01.eaalab.hpi.uni-potsdam.de/";
-              $scope.consumer.marketplace_url     = "http://vm-mpws2016hp1-04.eaalab.hpi.uni-potsdam.de/marketplace/";
+              $scope.consumer_url                 = "http://vm-mpws2016hp1-01.eaalab.hpi.uni-potsdam.de";
+              $scope.consumer.marketplace_url     = "http://vm-mpws2016hp1-04.eaalab.hpi.uni-potsdam.de/marketplace";
               $scope.consumer.tick                = 1;
               $scope.consumer.amount_of_consumers = 10;
               $scope.consumer.probability_of_sell = 50;
@@ -67,7 +67,7 @@
           co.controller('merchantCtrl', ['$route', '$routeParams', '$location', '$http', '$scope', '$cookieStore', '$window', '$filter', '$rootScope',
             function ($route, $routeParams, $location, $http, $scope, $cookieStore, $window, $filter, $rootScope) {
 
-              $scope.marketplace_url              = "http://vm-mpws2016hp1-04.eaalab.hpi.uni-potsdam.de/marketplace/";
+              $scope.marketplace_url              = "http://vm-mpws2016hp1-04.eaalab.hpi.uni-potsdam.de/marketplace";
               $scope.merchantConfig               = {};
 
               // Toastr options
@@ -81,7 +81,7 @@
               };
 
               $scope.getMerchants = function(){
-                $http.get($scope.marketplace_url + "merchants")
+                $http.get($scope.marketplace_url + "/merchants")
                     .then(function(response) {
                         $scope.merchants = response.data;
                         $scope.getDetails();
