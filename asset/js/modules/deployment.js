d456d91aecb4da3ede3f3de310c1896ad6ede8fe
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2016-11-22 16:52:47 +0100

    Remove double slashes for all services

--- asset/js/modules/deployment.js
+++ asset/js/modules/deployment.js
@@ -20,7 +20,7 @@
 
               $scope.merchant                     = {};
               $scope.merchant.merchant_url        = "http://vm-mpws2016hp1-06.eaalab.hpi.uni-potsdam.de";
-              $scope.merchant.marketplace_url     = "http://vm-mpws2016hp1-04.eaalab.hpi.uni-potsdam.de/marketplace/";
+              $scope.merchant.marketplace_url     = "http://vm-mpws2016hp1-04.eaalab.hpi.uni-potsdam.de/marketplace";
               $scope.merchant.name                = "Merchant der Erste";
               $scope.merchant.description         = "Cooler Merchant doing Work";
               $scope.merchant.nextState           = "init";
