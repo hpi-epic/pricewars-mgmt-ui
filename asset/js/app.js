a114c0dc6ba82bda68a83ee156f72ea356196523
Johanna Latt <Johanna Latt>
2017-01-24 13:27:04 +0100

    adds merchants-factory and uses it in the config-part

--- asset/js/app.js
+++ asset/js/app.js
@@ -92,4 +92,107 @@
             }
         };
     });
+
+    frontend.factory('data', ['$rootScope', function ($rootScope) {
+        var data = {
+            urls : {
+                marketplace_url : "http://vm-mpws2016hp1-04.eaalab.hpi.uni-potsdam.de:8080/marketplace",
+                producer_url    : "http://vm-mpws2016hp1-03.eaalab.hpi.uni-potsdam.de",
+                consumer_url    : "http://vm-mpws2016hp1-01.eaalab.hpi.uni-potsdam.de"
+            },
+            merchants : {},
+            products  : {},
+            consumer  : {}
+        };
+
+        return data;
+    }]);
+
+    // The merchant service. Stores all merchants currently registered
+    // at the marketplace and if requested, updates them periodically (not by default).
+    frontend.factory('merchants', ['$http', '$rootScope', function ($http, $rootScope) {
+        var marketplace_url = "http://vm-mpws2016hp1-04.eaalab.hpi.uni-potsdam.de:8080/marketplace";
+
+        var timeoutObj  = undefined;
+        var timeout     = -1;
+
+        var merchants   = {};
+
+        function getMerchants() {
+            $http.get(marketplace_url + "/merchants")
+                .then(function(response) {
+                    for (var key in response.data) {
+                        if (response.data.hasOwnProperty(key)) {
+                            var merchant = response.data[key];
+                            var merchantID = -1;
+                            for (var merch_key in merchant) {
+                                if (merch_key == "merchant_id") {
+                                    merchantID = merchant[merch_key];
+                                    delete(merchant[merch_key]);
+                                }
+                            }
+                            merchants[merchantID] = merchant;
+                        }
+                    }
+                    getMerchantDetails();
+
+                    // check for merchants every x seconds
+                    if (timeoutObj) clearTimeout(timeoutObj);
+                    if (timeout > 0) timeoutObj = setTimeout(getMerchants, timeout);
+                });
+        }
+
+        function getMerchantDetails() {
+            for (var merchantID in merchants) {
+                (function(merchant_id) {
+                    $http.get(merchants[merchant_id]["api_endpoint_url"] + "/settings")
+                        .then(function(response) {
+                            Object.keys(response.data).sort().forEach(function(key) {
+                                if (key != "merchant_id" && key != "merchant_url") {
+                                    merchants[merchant_id][key] = response.data[key];
+                                }
+                            });
+                        });
+                })(merchantID);
+            }
+        }
+
+        getMerchants();
+
+        return {
+            get: function(merchant_id) {
+                if (merchant_id) {
+                    return merchants[merchant_id];
+                }
+                return merchants;
+            },
+            getMerchantName: function(merchant_id) {
+                return ((merchant_id in merchants) && merchants[merchant_id].merchant_name) ? merchants[merchant_id].merchant_name : merchant_id.substring(0, 8);
+            },
+            isRegisteredMerchant: function(merchant_id) {
+                return (merchant_id in merchants);
+            },
+            setMerchantCheckTimeout: function(newTimeout) {
+                timeout = newTimeout;
+            },
+            enablePeriodicMerchantUpdate: function(enable, newTimeout) {
+                if (enable) {
+                    if (timeoutObj) clearTimeout(timeoutObj);
+                    if (newTimeout) {
+                        timeout = newTimeout;
+                    } else if (timeout < 1) {
+                        timeout = 10000;
+                    }
+                    timeoutObj = setTimeout(getMerchants, timeout);
+                } else {
+                    if (timeoutObj) clearTimeout(timeoutObj);
+                    timeout = -1;
+                }
+            },
+            updateMerchants: function() {
+                if (timeoutObj) clearTimeout(timeoutObj);
+                getMerchants();
+            }
+        };
+    }]);
 })();
