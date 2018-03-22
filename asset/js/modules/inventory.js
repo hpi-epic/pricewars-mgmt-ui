485b108aaa839e8b6066e3faaf42b713a3502245
Carsten Walther <carsten.walther@student.hpi.de>
2018-03-22 16:24:32 +0100

    update graph for each new data point

--- asset/js/modules/inventory.js
+++ asset/js/modules/inventory.js
@@ -1,95 +1,90 @@
 (function () {
-    var inventory = angular.module('inventory', ['ngCookies']);
+    const inventory = angular.module('inventory', ['ngCookies']);
 
     inventory.controller('inventoryController', ['$routeParams', '$location', '$http', '$scope', '$cookieStore', '$window', '$filter', '$rootScope', '$timeout', 'merchants', 'endpoints', 'charts', 'producer',
-            function ($routeParams, $location, $http, $scope, $cookieStore, $window, $filter, $rootScope, $timeout, merchants, endpoints, charts, producer) {
+        function ($routeParams, $location, $http, $scope, $cookieStore, $window, $filter, $rootScope, $timeout, merchants, endpoints, charts, producer) {
 
-                $scope.maxBulkSizeUpdate    = 25;
+            $scope.product_ids = charts.getCurrentProductIDs();
+            $scope.currentIDFilter = "1";
 
-                $scope.product_ids     = charts.getCurrentProductIDs();
-                $scope.currentIDFilter = "1";
+            $scope.charts = [];
 
-                $scope.charts = [];
+            /**
+             * UI settings
+             */
+            toastr.options = {
+                "debug": false,
+                "newestOnTop": false,
+                "positionClass": "toast-top-center",
+                "closeButton": true,
+                "toastClass": "animated fadeInDown",
+                "timeOut": "2000"
+            };
 
-                /**
-                  * UI settings
-                */
-                toastr.options = {
-                    "debug": false,
-                    "newestOnTop": false,
-                    "positionClass": "toast-top-center",
-                    "closeButton": true,
-                    "toastClass": "animated fadeInDown",
-                    "timeOut": "2000"
-                };
+            $('.showhide').click(function (event) {
+                event.preventDefault();
+                const hpanel = $(this).closest('div.hpanel');
+                const icon = $(this).find('i:first');
+                const body = hpanel.find('div.panel-body');
+                const footer = hpanel.find('div.panel-footer');
+                body.slideToggle(300);
+                footer.slideToggle(200);
 
-                $('.showhide').click(function (event) {
-                    event.preventDefault();
-                    var hpanel = $(this).closest('div.hpanel');
-                    var icon = $(this).find('i:first');
-                    var body = hpanel.find('div.panel-body');
-                    var footer = hpanel.find('div.panel-footer');
-                    body.slideToggle(300);
-                    footer.slideToggle(200);
+                // Toggle icon from up to down
+                icon.toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
+                hpanel.toggleClass('').toggleClass('panel-collapse');
+                setTimeout(function () {
+                    hpanel.resize();
+                    hpanel.find('[id^=map-]').resize();
+                }, 50);
+            });
 
-                    // Toggle icon from up to down
-                    icon.toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
-                    hpanel.toggleClass('').toggleClass('panel-collapse');
-                    setTimeout(function () {
-                        hpanel.resize();
-                        hpanel.find('[id^=map-]').resize();
-                    }, 50);
-                });
+            function drawPriceGraphs() {
+                $scope.charts["highchart-inventory"] = Highcharts.stockChart(charts.inventory.html_id, charts.inventory.getOptions());
+                charts.setSize($scope.charts["highchart-inventory"], undefined, 600);
+            }
 
-                function drawPriceGraphs() {
-                    /* --- Price Updates and Item Sales --- */
-                    $scope.charts["highchart-inventory"] = Highcharts.stockChart(charts.inventory.html_id, charts.inventory.getOptions());
-                    charts.setSize($scope.charts["highchart-inventory"], undefined, 600);
-                }
+            let bulkUpdateOfferUpdate = [];
 
-                var bulkUpdateOfferUpdate = [];
+            function updateGraphWithPriceUpdate() {
+                charts.inventory.updateGraphWithPriceData($scope.charts["highchart-inventory"], bulkUpdateOfferUpdate, $scope.currentIDFilter);
+                bulkUpdateOfferUpdate = [];
+                $scope.$digest();
+            }
 
-                function updateGraphWithPriceUpdate() {
-                    if (bulkUpdateOfferUpdate.length >= $scope.maxBulkSizeUpdate) {
-                        charts.inventory.updateGraphWithPriceData($scope.charts["highchart-inventory"], bulkUpdateOfferUpdate, $scope.currentIDFilter);
-                        bulkUpdateOfferUpdate = [];
-                        $scope.$digest();
-                    }
-                }
+            /**
+             * Handling socket events
+             */
+            endpoints.getData().then(function (urls) {
+                $scope.consumer_url = urls.consumer_url;
+                $scope.marketplace_url = urls.marketplace_url;
+                $scope.producer_url = urls.producer_url;
+                $scope.kafka_proxy = urls.kafka_proxy;
 
-                /**
-                  * Handling socket events
-                */
-                endpoints.getData().then(function(urls){
-                   $scope.consumer_url   = urls.consumer_url;
-                   $scope.marketplace_url= urls.marketplace_url;
-                   $scope.producer_url   = urls.producer_url;
-                   $scope.kafka_proxy    = urls.kafka_proxy;
+                merchants.loadMerchants().then(function () {
+                    drawPriceGraphs();
 
-                    merchants.loadMerchants().then(function() {
-                       drawPriceGraphs();
+                    const socket = io.connect($scope.kafka_proxy, {query: 'id=mgmt-ui'});
 
-                       var socket = io.connect($scope.kafka_proxy, {query: 'id=mgmt-ui'});
+                    socket.on('updateOffer', function (data) {
+                        data = angular.fromJson(data);
 
-                       socket.on('updateOffer', function (data) {
-                           data = angular.fromJson(data);
+                        if (data instanceof Array) {
+                            bulkUpdateOfferUpdate = bulkUpdateOfferUpdate.concat(data);
+                        } else {
+                            bulkUpdateOfferUpdate.push(data);
+                        }
 
-                           if (data instanceof Array) {
-                               bulkUpdateOfferUpdate = bulkUpdateOfferUpdate.concat(data);
-                           } else {
-                               bulkUpdateOfferUpdate.push(data);
-                           }
-
-                           updateGraphWithPriceUpdate();
-                       });
-
-                       $scope.$on('$locationChangeStart', function() {
-                           socket.disconnect();
-                       });
-
+                        updateGraphWithPriceUpdate();
                     });
-                });
 
-            }] //END: controller function
+                    $scope.$on('$locationChangeStart', function () {
+                        socket.disconnect();
+                    });
+
+                });
+            });
+
+        }] //END: controller function
     );  // END: dashboardController
 })(); //END: global function
